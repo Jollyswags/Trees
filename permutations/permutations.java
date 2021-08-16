ec6571c6f89150eddda0a34a5c2036a3575f830a
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-08-16 21:00:49 +0530

    Time: 1 ms (92.55%), Space: 38.8 MB (98.93%) - LeetHub

---
+++ permutations/permutations.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        boolean visited[]=new boolean[nums.length];
+        helper(nums,0,nums.length,new ArrayList<>(),res,visited);
+        return res;
+        
+    }
+    public void helper(int nums[],int pos,int len,List<Integer> l,List<List<Integer>> res,boolean visited[])
+    {
+        if(l.size()==len)
+        {
+            res.add(new ArrayList<>(l));
+            return;
+        }
+        if(pos==nums.length)
+            return;
+        for(int i=pos;i<nums.length;i++)
+        {
+           if(!visited[i])
+           {
+               visited[i]=true;
+                l.add(nums[i]);
+            helper(nums,pos,len,l,res,visited);
+            l.remove(l.size()-1);
+               visited[i]=false;
+           }
+        }
+    }
+}
